65c7e45eb1478ff458b7d827a694ee3551297c37
Balaji5755 <137764386+Balaji5755@users.noreply.github.com>
2024-10-17 18:46:04 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Balaji");
+		System.out.println("How are you");
+		System.out.println("Java learner");
+
+	}
+
+}
